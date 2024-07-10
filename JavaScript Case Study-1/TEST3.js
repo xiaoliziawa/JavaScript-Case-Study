ea218ce5b7833017c961d402c8168e24ff4fb96c
xiaoliziawa <xiaoliziawa@gamil.com>
2024-07-11 01:47:50 +0800

    some changes

--- JavaScript Case Study-1/TEST3.js
+++ JavaScript Case Study-1/TEST3.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
+const { userInfo } = require('os');
 const path = require('path');
 
 const fileName = 'example.txt';
 const filePath = path.join(__dirname, 'example_txt', fileName);
 
-const content = `我是一个TXT文本鸭,\n现在是:${new Date().toLocaleString()}`
+const content = `我是一个TXT文本鸭,\n现在是:${new Date().toLocaleString()},\n${new Date().toUTCString()}`
 
-fs.mkdirSync(path.dirname(filePath), { recursive: true });
 
 fs.writeFile(filePath, content, 'utf8', (err) => {
     if (err) {
